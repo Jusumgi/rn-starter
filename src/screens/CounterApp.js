ba9df4e6692df22f3612a742f4406d25c20f9099
Jusumgi <auronold@gmail.com>
2025-01-02 12:48:24 -0500

    reducer exercise

--- src/screens/CounterApp.js
+++ src/screens/CounterApp.js
@@ -1,27 +1,35 @@
 // state example working with numbers
 
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
+const INCREMENT = 5;
+
+const reducer = (state, action) => {
+  console.log(action);
+  return { ...state, count: state.count + action.amount };
+};
 
 const CounterApp = () => {
-  const [counter, setCounter] = useState(0);
+  // const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  const { count } = state;
+  // console.log(count);
 
   return (
     <View>
       <Button
         title="Increase"
         onPress={() => {
-          //   counter++;
-          setCounter(counter + 1);
+          dispatch({ count, amount: INCREMENT });
         }}
       />
       <Button
         title="Decrease"
         onPress={() => {
-          setCounter(counter - 1);
+          dispatch({ count, amount: -1 * INCREMENT });
         }}
       />
-      <Text>Current Count: {counter}</Text>
+      <Text>Current Count: {count}</Text>
     </View>
   );
 };
